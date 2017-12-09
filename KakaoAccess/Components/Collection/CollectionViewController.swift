1e4e84598ae65bc9617601a6dd971c8dff95c571
Fabian Ehlert <fabian@fabianehlert.com>
2017-12-09 11:53:33 +0100

    Adds Screens

---
+++ KakaoAccess/Components/Collection/CollectionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewController.swift
+//  KakaoAccess
+//
+//  Created by Fabian Ehlert on 09.12.17.
+//  Copyright © 2017 Fabian Ehlert. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.navigationController?.navigationBar.prefersLargeTitles = true
+	}
+	
+}
---
+++ KakaoAccess/Components/Table/TableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TableViewController.swift
+//  KakaoAccess
+//
+//  Created by Fabian Ehlert on 09.12.17.
+//  Copyright © 2017 Fabian Ehlert. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.navigationController?.navigationBar.prefersLargeTitles = true
+	}
+	
+}
