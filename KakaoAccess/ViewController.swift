674fae5e2eec84a15aedc1d8f798fb25a9483575
Fabian Ehlert <fabian@fabianehlert.com>
2017-12-09 10:44:58 +0100

    Initial Commit

---
+++ KakaoAccess/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KakaoAccess
+//
+//  Created by Fabian Ehlert on 09.12.17.
+//  Copyright © 2017 Fabian Ehlert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
