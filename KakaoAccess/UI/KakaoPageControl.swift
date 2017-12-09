a02d5ebc1bc3b9907cd0625f405192959bcccfed
Fabian Ehlert <fabian@fabianehlert.com>
2017-12-09 11:35:14 +0100

    Adds page control and moves files

---
+++ KakaoAccess/UI/KakaoPageControl.swift
@@ -0,0 +1,68 @@
+//
+//  KakaoPageControl.swift
+//  KakaoAccess
+//
+//  Created by Fabian Ehlert on 09.12.17.
+//  Copyright © 2017 Fabian Ehlert. All rights reserved.
+//
+
+import UIKit
+
+class KakaoPageControl: UIView {
+	
+	private enum Const {
+		static let innerMargin: CGFloat = 10
+		
+		static let inactiveColor = UIColor.lightGray.cgColor
+		static let activeColor = UIColor.black.cgColor
+	}
+	
+	var currentPage = 0 {
+		willSet {
+			assert(newValue <= self.numberOfPages-1)
+		}
+		didSet {
+			for (i, indicator) in self.indicators.enumerated() {
+				indicator.fillColor = (i == self.currentPage) ? Const.activeColor : Const.inactiveColor
+			}
+		}
+	}
+	private var numberOfPages: Int
+	
+	private var indicators = [CAShapeLayer]()
+	
+	// MARK: - Init
+	
+	init(frame: CGRect, numberOfPages: Int) {
+		self.numberOfPages = numberOfPages
+		
+		super.init(frame: frame)
+		
+		for _ in 0..<numberOfPages {
+			let indicator = CAShapeLayer()
+			self.layer.addSublayer(indicator)
+			self.indicators.append(indicator)
+		}
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	// MARK: -
+	
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		
+		for (i, indicator) in self.indicators.enumerated() {
+			let w = ((self.frame.width / CGFloat(self.numberOfPages)) - Const.innerMargin) + (Const.innerMargin/CGFloat(self.numberOfPages))
+			indicator.path = UIBezierPath(roundedRect: CGRect(x: (CGFloat(i) * (w + Const.innerMargin)),
+															  y: 0,
+															  width: w,
+															  height: self.frame.height),
+										  cornerRadius: self.frame.height / 2).cgPath
+			indicator.fillColor = i == self.currentPage ? Const.activeColor : Const.inactiveColor
+		}
+	}
+	
+}
