a537e64a8eea481684db1d1c0db03665b0b98456
Fabian Ehlert <fabian@fabianehlert.com>
2017-12-09 13:29:38 +0100

    Adds data to TableViewController

--- KakaoAccess/Components/Table/TableViewController.swift
+++ KakaoAccess/Components/Table/TableViewController.swift
@@ -10,9 +10,38 @@ import UIKit
 
 class TableViewController: UITableViewController {
 	
+	var aircraft: [Aircraft]?
+	
+	// MARK: - ViewController lifecycle
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		self.navigationController?.navigationBar.prefersLargeTitles = true
+		
+		self.aircraft = Parser.parseAircraft(file: "Aircraft.json")
+		self.tableView.reloadData()
+	}
+	
+}
+
+// MARK: - UITableViewDataSource, UITableViewDelegate
+extension TableViewController {
+	
+	override func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return aircraft?.count ?? 0
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		if let cell = self.tableView.dequeueReusableCell(withIdentifier: "TableCell", for: indexPath) as? TableCell {
+			cell.viewModel = AircraftViewModel(aircraft: self.aircraft?[indexPath.row])
+			cell.selectionStyle = .none
+			return cell
+		}
+		return UITableViewCell()
 	}
 	
 }
